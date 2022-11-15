7b186a597a586aefb8cb52a7b0d442224713d0c6
Olivia Radick <olivia.radick@gmail.com>
2022-11-15 16:46:58 -0500

    commit tree 978da0ffa71c217b42ba951a512ddc8f5eb59dc2

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    function clickAlert(){
+    alert('I was clicked!')};
+
+    input.addEventListener('click', clickAlert);
 }
+const input = document.getElementById('button');
+
+
+
